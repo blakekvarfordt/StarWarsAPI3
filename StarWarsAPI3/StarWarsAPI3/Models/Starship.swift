ff1049fb35fff69ebc5b8711c616d5b1ab9d3574
Blake Kvarfordt <blakekvarfordt@gmail.com>
2019-10-03 17:58:59 -0600

    Sets up Starship Model

---
+++ StarWarsAPI3/StarWarsAPI3/Models/Starship.swift
@@ -0,0 +1,30 @@
+//
+//  Starship.swift
+//  StarWarsAPI3
+//
+//  Created by Blake kvarfordt on 10/3/19.
+//  Copyright © 2019 Blake kvarfordt. All rights reserved.
+//
+
+import Foundation
+
+struct TopLevelDict: Decodable {
+    let starships: [Starship]
+}
+
+struct Starship: Decodable {
+    let name: String
+    let model: String
+    let cost: String
+    let speed: String
+    let films: [String]
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case model
+        case cost = "cost_in_credits"
+        case speed = "max_atmosphering_speed"
+        case films
+    }
+    
+}
