4106b936c23901ade60edec31afdecdfd76bd156
Blake Kvarfordt <blakekvarfordt@gmail.com>
2019-10-03 18:49:02 -0600

    Sets up GetDaShips function in Starship Controller

--- StarWarsAPI3/StarWarsAPI3/Model Controllers/StarshipController.swift
+++ StarWarsAPI3/StarWarsAPI3/Model Controllers/StarshipController.swift
@@ -10,5 +10,34 @@ import Foundation
 
 class StarshipController {
     
+    let baseURL = URL(string: StarshipConstants.baseURL)
     
+    func getDaStarships(with searchTerm: String, completion: @escaping ([Starship]) -> Void ) {
+        
+        guard let url = baseURL else { completion([]); return }
+        
+        var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: true)
+        
+        let queryItem = URLQueryItem(name: StarshipConstants.queryItem1, value: searchTerm)
+        
+        urlComponents?.queryItems = [queryItem]
+        
+        guard let finalURL = urlComponents?.url else { completion([]); return }
+        
+        let dataTask = URLSession.shared.dataTask(with: finalURL) { (data, _, error) in
+            
+            if let error = error {
+                print("Error", error.localizedDescription)
+            }
+            
+            guard let data = data else { completion ([]); return }
+            
+            do {
+                let decodedwhatever = try JSONDecoder().decode(TopLevelDict.self, from: data)
+                completion(decodedwhatever.starships)
+            } catch {
+                completion([])
+            }
+        }; dataTask.resume()
+    }
 }
