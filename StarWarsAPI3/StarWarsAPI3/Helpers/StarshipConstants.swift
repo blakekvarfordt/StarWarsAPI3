d69631d5f2a301ef729dde623973c08a63d5fe26
Blake Kvarfordt <blakekvarfordt@gmail.com>
2019-10-03 18:28:08 -0600

    Sets up Storyborads / StarshipConstants

---
+++ StarWarsAPI3/StarWarsAPI3/Helpers/StarshipConstants.swift
@@ -0,0 +1,14 @@
+//
+//  StarshipConstants.swift
+//  StarWarsAPI3
+//
+//  Created by Blake kvarfordt on 10/3/19.
+//  Copyright © 2019 Blake kvarfordt. All rights reserved.
+//
+
+import Foundation
+
+struct StarshipConstants {
+    static let baseURL = "https://swapi.co/api/starships/"
+    static let queryItem1 = "search"
+}
---
+++ StarWarsAPI3/StarWarsAPI3/Model Controllers/StarshipController.swift
@@ -0,0 +1,14 @@
+//
+//  StarshipController.swift
+//  StarWarsAPI3
+//
+//  Created by Blake kvarfordt on 10/3/19.
+//  Copyright © 2019 Blake kvarfordt. All rights reserved.
+//
+
+import Foundation
+
+class StarshipController {
+    
+    
+}
